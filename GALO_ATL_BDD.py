7655ba631538e387d7a37b3405d28ce1494c5342
Arthur Cordeiro Andrade <arthurcoand@gmail.com>
2023-03-25 08:37:03 -0300

    🐔

---
+++ GALO_ATL_BDD.py
@@ -0,0 +1,10 @@
+# "A LICENÇA AÇAÍWARE" (Revisão +55):
+# arthurcoand@gmail.com escreveu este arquivo em 2023.
+# Enquanto você manter este comentário, você poderá fazer o que quiser com este arquivo.
+# Caso nos encontremos algum dia e você ache que este arquivo vale, você poderá me comprar uma açaí em retribuição.
+# Arthur Cordeiro Andrade.
+
+from BDD.ATL_BDD import ATL_BDD
+
+if __name__ == "__main__":
+    ATL_BDD()
---
+++ README.md
@@ -0,0 +1,3 @@
+# GALO 🐔 Gerador de Análises Livre e Open-source
+
+<a href="LICENSE">![Badge](https://img.shields.io/badge/license-AÇAÍWARE-purple?style=for-the-badge)</a>
